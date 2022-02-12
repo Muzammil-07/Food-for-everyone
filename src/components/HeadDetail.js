b2093f743b6a8f9a8d35fd4d59e40908df6cf77f
Muzammil <shanikbs6242@gmail.com>
2022-02-12 18:17:58 +0500

    code indentation

--- src/components/HeadDetail.js
+++ src/components/HeadDetail.js
@@ -1,18 +1,17 @@
 import thumb from '../images/thumb.png'
 import Logo from '../images/logo.png'
-const HeadDetail=()=>{
-    return(
-       <div style={{ display:'flex' ,flexDirection:'row'}}>
-        <div className="text-2xl text-orange-300  mt-6 pt-10">
-            <div></div>
-      <h1 >
-     Sylani Khana Sab kilye Service
-      </h1>
-      <p>  Providing Rashan  <br/>All Over Karachi.</p>
-      </div>
-     <img src={Logo} className='h-50'/>
-     
-      </div>
+const HeadDetail = () => {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <div className="text-2xl text-orange-300  mt-6 pt-10">
+                <div></div>
+                <h1 >
+                    Sylani Khana Sab kilye Service
+                </h1>
+                <p>  Providing Rashan  <br />All Over Karachi.</p>
+            </div>
+            <img src={Logo} className='h-50' />
+        </div>
     )
 }
 export default HeadDetail;
